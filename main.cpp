5a071d4f20c931f968911ccb96974cfbe86b2406
coryperr <coryperr@88ce85bb-8564-4ae6-884f-ab7503230054>
2012-07-10 18:13:38 +0000

    Updated gui command line help with solver info
    git-svn-id: svn://innoculant.micro.med.umich.edu/dev/gr2d/GR-ABM-ODE@1529 88ce85bb-8564-4ae6-884f-ab7503230054

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
 		("diffusion", po::value<int>(&diffMethod)->default_value(4),
      "Diffusion method:\n0 - FTCS\n1 - BTCS (SOR, correct)\n2 - BTCS (SOR, wrong)\n3 - FTCS Grid Swap\n4 - ADE Grid Swap")
     ("odesolver", po::value(&odeSolver)->default_value(0),
-     "ODE Solver Method:\n0 - Forward Euler\n1 - Runge-Kutta 4th Order\n2 - Runge-Kutta 2nd Order\n3 - Euler Predictor-Corrector")
+     "ODE Solver Method:\n0 - Forward Euler\n1 - Euler Predictor-Corrector\n2 - Runge-Kutta 3rd Order\n3 - Runge-Kutta 4th Order\n4 - HuenEuler\n5 - Runge-Kutta Cache-Karp\n6 - Runge-Kutta Fehlberg\n7 - Bogacki-Shampine")
 		("timesteps,t", po::value<int>(&timesteps), "Number of time steps to simulate\nTakes precedence over --days")
 		("days", po::value<int>(&nDays)->default_value(200), "Number of days to simulate")
 		("script,c", "Scripting mode")
